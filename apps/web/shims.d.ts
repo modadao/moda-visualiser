a485dca0748d728f4ff969e13b9d6325edfd3b9f
connorgmeean <a@cgm.codes>
2022-06-15 12:09:00 +1000

    refact: Removed turborepo

--- apps/web/shims.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.mp3' {
-  const s = '';
-  export default s;
-}
-declare module '*.glsl' {
-  const s = '';
-  export default s;
-}
--- packages/moda-visualiser/LICENSE.md
+++
@@ -1,1 +0,0 @@
-(c) Copyright 2021 MODA DAO (modadoa.io), all rights reserved.
